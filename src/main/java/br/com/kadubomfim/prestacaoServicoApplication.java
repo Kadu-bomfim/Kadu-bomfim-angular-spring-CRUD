11679b9e9ed119a4b20a871273b8371964dd2fac
kadu bomfim <kaduebb@gmail.com>
2021-09-28 22:08:32 -0300

    criando projeto Versão: 1.0.0

---
+++ src/main/java/br/com/kadubomfim/prestacaoServicoApplication.java
@@ -0,0 +1,12 @@
+package br.com.kadubomfim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class prestacaoServicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(prestacaoServicoApplication.class, args);
+    }
+}
